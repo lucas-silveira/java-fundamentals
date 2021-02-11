a4a50e7624158c6fb3f9bb4f3c9ee266f0e03ebf
Lucas Silveira <lugano1995@gmail.com>
2021-02-11 16:45:17 -0300

    create comments java

---
+++ Fundamentals/src/Comments.java
@@ -0,0 +1,17 @@
+/**
+ * javadoc
+ * Esta classe é apenas um wrapper para mostrarmos o funcionamento do javadoc e comentários.
+ *
+ * @author lucassilveira
+ */
+
+public class Comments {
+    public static void main(String[] args) {
+        // one line comment
+
+        /*
+        Line 1
+        Line 2
+         */
+    }
+}

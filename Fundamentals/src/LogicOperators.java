3119609cd69a981c3593f8e5145fba009b0ad073
Lucas Silveira <lugano1995@gmail.com>
2021-02-13 02:00:11 -0300

    refactor logic operators

--- Fundamentals/src/LogicOperators.java
+++ Fundamentals/src/LogicOperators.java
@@ -2,8 +2,14 @@ public class LogicOperators {
     public static void main(String[] args) {
         boolean resp;
 
-        resp = true && true; // true
-        resp = true || false; // true
-        resp = !true; // false
+        // and
+        System.out.println(true && true); // true
+        // or
+        System.out.println(true || false); // true
+        // xor
+        System.out.println(true ^ true); // false
+
+        System.out.println(!true); // false
+        System.out.println(!!true); // true
     }
 }

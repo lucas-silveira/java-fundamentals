e083f09da2b7d4bf083ede583a89a72a0680278f
Lucas Silveira <lugano1995@gmail.com>
2021-02-13 02:02:24 -0300

    refactor relational operators

--- Fundamentals/src/RelationalOperators.java
+++ Fundamentals/src/RelationalOperators.java
@@ -2,11 +2,11 @@ public class RelationalOperators {
     public static void main(String[] args) {
         boolean resp;
 
-        resp = true == true; // true
-        resp = true != false; // true
-        resp = 2 > 1; // true
-        resp = 2 >= 1; // true
-        resp = 2 < 1; // false
-        resp = 2 <= 1; // false
+        System.out.println(true == true); // true
+        System.out.println(true != false); // true
+        System.out.println(2 > 1); // true
+        System.out.println(2 >= 1); // true
+        System.out.println(2 < 1); // false
+        System.out.println(2 <= 1); // false
     }
 }

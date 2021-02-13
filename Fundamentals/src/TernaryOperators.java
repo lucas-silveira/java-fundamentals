b928f8978ebf5180c396588891295db143288651
Lucas Silveira <lugano1995@gmail.com>
2021-02-13 02:10:09 -0300

    ternary operators

---
+++ Fundamentals/src/TernaryOperators.java
@@ -0,0 +1,8 @@
+public class TernaryOperators {
+    public static void main(String[] args) {
+        double average = 7.6;
+        String result = average >= 7.0 ? "Approved" : "Denny";
+
+        System.out.println(result); // Approved
+    }
+}
